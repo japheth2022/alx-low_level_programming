7aa77959c8681bb00a533a1de296e03932131be3
root <japhethmuli.mj@gmail.com>
2022-07-06 09:50:28 -0700

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,8 +9,10 @@ void times_table(void)
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
-	for (j = 0; i <= 9; j++)
 	{
-		_putchar((i * j));
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar((i * j));
+		}
 	}
 }
