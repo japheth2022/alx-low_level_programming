c9e7a49719be48c4dfa7e56868dd45bf70d5c5fd
root <japhethmuli.mj@gmail.com>
2022-07-07 10:16:17 -0700

    is ad digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks if a character is a digit
+ * @c: is a parameter
+ * Return: success
+ */
+
+
+
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
