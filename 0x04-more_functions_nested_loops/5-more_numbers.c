823396d6f7a3bdc6e42843a46c579702a1e36f86
root <japhethmuli.mj@gmail.com>
2022-07-07 11:22:35 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints more numbers
+ */
+
+
+
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+		_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
