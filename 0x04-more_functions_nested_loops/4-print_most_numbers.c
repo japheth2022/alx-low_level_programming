05fdf78fd3ee0627f328e90618c39b4331938451
root <japhethmuli.mj@gmail.com>
2022-07-07 11:09:12 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,16 +4,14 @@
  */
 
 
-
-
 void print_most_numbers(void)
 {
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (x != 2 && x != 4)
-			_putchar(x + '0');
+		if (x != 2 && x != 4)
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
