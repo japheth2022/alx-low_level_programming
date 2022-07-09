c1d2261816ad8732d42e6a677b0b93682cf9bce0
root <japhethmuli.mj@gmail.com>
2022-07-09 05:36:20 -0700

    lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints lower case alphabets
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
