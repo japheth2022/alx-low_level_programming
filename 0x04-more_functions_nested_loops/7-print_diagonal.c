4bba43558447a86c7a0cd5f0ea5ae7d4b7c9851b
root <japhethmuli.mj@gmail.com>
2022-07-12 12:16:10 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,15 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 	_putchar('\n');
+
 	else
 	{
-	for (x = 0; x < n; x++)
+		for (x = 0; x < n; x++)
 		{
 		for (i = 0; i < x; i++)
 			_putchar(' ');
-			_putchar(92);
-			_putchar('\n');
-
+		_putchar(92);
+		_putchar('\n');
 		}
 	}
-
 }
