123db930658775c168fc9580e6365655fb345ca4
root <japhethmuli.mj@gmail.com>
2022-07-15 05:46:01 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compares strings
+ * @s1:parameter 1
+ * @s2: parameter 2
+ * Return: integer
+ */
+
+
+
+
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
