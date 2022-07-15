a0c0159b5cad51a7bfb5c43e63d06f394c22d349
root <japhethmuli.mj@gmail.com>
2022-07-15 14:50:52 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: integer parameter
+ * Return: integer
+ */
+
+
+
+
+void print_number(int n)
+{
+	for (n >= 48; n <= 57; n++)
+
+		_putchar(n);
+	return (n);
+}
