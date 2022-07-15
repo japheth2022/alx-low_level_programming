e9a7c50e01723eeb4d8623a98411737b40f689ec
root <japhethmuli.mj@gmail.com>
2022-07-15 04:30:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,8 +16,8 @@ char *_strcat(char *dest, char *src)
 	int len = 0, i;
 
 	while (dest[len])
-		len++
-	for (i = 0, src[i] != 0; i++)
+		len++;
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
