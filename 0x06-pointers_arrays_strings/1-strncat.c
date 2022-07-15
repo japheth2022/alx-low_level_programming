1c75f8a24e2d81b14d8b27cb7b1583d58f1b4e2d
root <japhethmuli.mj@gmail.com>
2022-07-15 04:50:54 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - function to append characters
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ * Return: string
+ */
+
+
+
+
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest; src; n);
+	return (dest);
+}
