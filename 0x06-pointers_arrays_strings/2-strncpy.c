be7271b9813b2bf2b0cb14aa3932ba7c8be4ff8a
root <japhethmuli.mj@gmail.com>
2022-07-15 05:27:38 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - function that copies strings
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n:parameter 3
+ * Return: string
+ */
+
+
+
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
