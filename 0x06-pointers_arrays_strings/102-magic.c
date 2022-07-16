cd0421d09485ffb8a1dad927a451c95fbb72a01d
root <japhethmuli.mj@gmail.com>
2022-07-15 22:24:36 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
-/**
- * main - prints elements of an array
- * Return: always 0
- */
-
-
 
 int main(void)
-
 {
-
 	int n;
-
 	int a[5];
-
 	int *p;
 
-
-
 	a[2] = 1024;
-
 	p = &n;
-
-	 /*
+	/*
 	* write your line of code here...
 	* Remember:
 	* - you are not allowed to use a
@@ -30,13 +16,8 @@ int main(void)
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
-
 	a[2] = 98;
-
 	/* ...so that this prints 98\n */
-
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
-
 }
