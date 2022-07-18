399ba04ae4139d164d15a858e394becf48cebea8
root <japhethmuli.mj@gmail.com>
2022-07-18 11:30:27 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strchr - character search function
+ * @s: string
+ * @c: character
+ * Return: a string
+ */
+
+
+
+
+
+
+
+char *_strchr(char *s, char c)
+
+{
+	int a = 0, b;
+
+	while (s[a])
+
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+		return (s);
+	}
+	return ('\0');
+}
