abccc9765c3f48dc5b5d7af5ce0529053024fba2
root <japhethmuli.mj@gmail.com>
2022-07-18 10:20:48 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _memcpy - memory coping function
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: string
+ */
+
+
+
+
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+		memcpy(src, dest);
+
+	return (dest);
+}
