4b72fcb63445b259c2b61cf8411583c24bafe913
root <japhethmuli.mj@gmail.com>
2022-07-19 09:26:49 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,15 +18,12 @@
 char *_strchr(char *s, char c)
 {
 	int a = 0, b;
-	while (s[a])
-	a++;
+
+	for (b = 0 ; b <= a; b++)
 	{
-		if (c === s[a])
+		if (c == s[b])
 		s += b;
 		return (s);
-
-
 	}
 	return (NULL);
 }
-
