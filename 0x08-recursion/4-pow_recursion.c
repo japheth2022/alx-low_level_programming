8021c970984e68988fb362284c501940ed5bdc40
root <japhethmuli.mj@gmail.com>
2022-07-20 15:07:44 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <math.h> 
+/**
+ * _pow_recursion - return the value of x raised to y
+ * @x: integer 1
+ * @y: integer 2
+ * Return: number
+ */
+
+
+
+
+
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	return (pow(x, y));
+}
