f21741bf7d046cf9ab1ae58c650e421d63c55f32
root <japhethmuli.mj@gmail.com>
2022-07-25 14:08:58 -0700

    prints number of passed arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,9 @@
  * main - prints the number of passed arguments
  * @argc: argc parameter
  * @argv: array of commands
- * Return: 0 for success
+ * Return: 0 for succes
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc , char *argv[]  __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
