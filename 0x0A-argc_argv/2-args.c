08b753ca1c3113b31fc394ae72b94e0badc3375c
root <japhethmuli.mj@gmail.com>
2022-07-25 14:38:05 -0700

    prints all arguments passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints al the passed arguments
+ * @argc: argc parameter
+ * @argv: array of listed commands
+ * Return: 0 for a success
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("argv index %d is = %s\n", count, argv[count]);
+		return (0);
+	}
+}
