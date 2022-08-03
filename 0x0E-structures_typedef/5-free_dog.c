fbcd6120d950b598bac64bde684a0c422ee9d70e
root <japhethmuli.mj@gmail.com>
2022-08-03 00:41:43 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,21 +8,20 @@
  * free_dog - function
  * @d: pointer to dog
  * Description: function to free dogs
- * Return: 0 Success
+ * Return: no return.
  */
 
 void free_dog(dog_t *d)
 
 {
 
-	if (d == NULL)
-
-	return;
-
+	if (d)
+	{
 	free(d->name);
 
 	free(d->owner);
 
 	free(d);
+	}
 
 }
