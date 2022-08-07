67f06d2198a31ab93660482f4973fadb8e8ea189
root <japhethmuli.mj@gmail.com>
2022-08-06 17:45:37 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,45 +1,64 @@
 #include "main.h"
+
 #include <stdlib.h>
-#include <stddef.h>
+
+#include <stdio.h>
+
+
+
 /**
- * alloc_grid - allocates 2D array
- * @width: width array
- * @height: height array
- * Return: a pointer to the allocated array
+ * alloc_grid - make a 2-D array dynamically
+ * @col: column of the array
+ * @row: row of the array
+ * Return: pointer to the 2-D array
  */
 
-int **alloc_grid(int width, int height)
+
+
+int **alloc_grid(int col, int row)
+
 {
-	int i, k, l, j;
-	int **a;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-	a = malloc(height * sizeof(int *));
+	int i, j,  **grid;
 
-	if (a == NULL)
-	{
-		free(a);
-		return (NULL);
-	for (i = 0; i < height; i++)
-	{
-		a[i] = malloc(width * sizeof(int));
-		if (a[i] == NULL)
-		{
-			for (j = i; j >= 0; j--)
-			{
-				free(a[j]);
-			}
-			free(a);
-			return (NULL);
 
-		}
-	}
-	}
-	for (k = 0; k < height; k++)
+
+	grid = malloc(row * sizeof(*grid));
+
+
+
+	if (row < 1 || col < 1 || grid == 0)
+
+	return (NULL);
+
+
+
+	for (i = 0; i < row; i++)
+
 	{
-		for (l = 0; l < width; l++)
-			a[l][k] = 0;
+
+	grid[i] = malloc(col * sizeof(**grid));
+
+	if (grid[i] == 0)
+
+	while (i--)
+
+	free(grid[i]);
+
+	free(grid);
+
+	return (NULL);
+
 	}
-	return (a);
+
+	for (j = 0; j < col; j++)
+
+	grid[i][j] = 0;
+
+	}
+
+
+
+	return (grid);
+
 }
