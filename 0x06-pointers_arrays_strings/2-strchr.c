711eaf1dff3866b5f76b8192e541f679c67aa29c
root <japhethmuli.mj@gmail.com>
2022-08-07 02:03:26 -0700

    2-strchr.c

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -17,6 +17,22 @@ char *_strchr(char *s, char c)
 
 {
 
-	return (strchr(s, c));
+	int i;
+
+
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	{
+
+	if (s[i] == c)
+
+	return (s + i);
+
+	}
+
+
+
+	return (NULL);
 
 }
