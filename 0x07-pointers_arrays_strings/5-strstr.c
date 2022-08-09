8d019c0e5d5ab691ee3692e88cba1d2c83ade230
root <japhethmuli.mj@gmail.com>
2022-08-09 03:55:06 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,47 +9,28 @@
  */
 char *_strstr(char *s, char *c)
 {
-
 	int len = strlen(c);
-
 	int i, count = 0;
 
 	if (len < 1)
-
 	return (NULL);
 
 	for (i = 0; s[i] != '\0'; i++)
-
 	{
-
 	if (count == len)
-
 	break;
 
-
-
 	if (c[count] == s[i])
-
 	count++;
 
 	else
-
 	{
-
 	if (count > 0)
-
 	i -= count;
-
 	count = 0;
-
 	}
-
 	}
-
 	if (count < len)
-
 	return (NULL);
-
 	return (s + (i - count));
-
 }
