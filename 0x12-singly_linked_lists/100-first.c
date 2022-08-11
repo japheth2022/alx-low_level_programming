ec102c09167abeb1704383801758a1d49ca7300f
root <japhethmuli.mj@gmail.com>
2022-08-11 10:38:27 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * the_hare - function
+ * Return: nothing
+ */
+
+void the_tortoise(void) __attribute__ ((constructor));
+
+void the_tortoise(void)
+
+{
+
+	printf("You're beat! and yet, you must allow,\n");
+
+	printf("I bore my house upon my back!\n");
+
+}
