ef2f860053165b87ac3da54584a1e6bb10510c0f
root <japhethmuli.mj@gmail.com>
2022-08-11 10:51:07 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -41,7 +41,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
-void the_tortoise(void) __attribute__ ((constructor));
+void the_tortoise(void);
 
 
 
