b8036b70b3c58abd027abc120b25d17091629c69
root <japhethmuli.mj@gmail.com>
2022-08-22 09:33:21 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+#endif
